7ce70640dd4ea107c48fc82a549fcd80036d16b0
Daniel Wetteroth <danielcw@gmail.com>
2015-11-09 12:41:17 -0500

    updates for modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,11 @@
 
 var currentSlide = 1;
 var currentSlideB = 1;
+var currentSlide3 = 1;
+var currentSlide4 = 1;
+var currentSlide5 = 1;
+var currentSlide6 = 1;
+
 
  $(document).ready(function () {
 
@@ -90,11 +95,84 @@ var currentSlideB = 1;
     });
 
 
+    $('.lnk3').on('click', function() {
+      $('#largeSlide3').modal('show');
+      $('.cplan3-main').attr('src','/assets/Plan3_Lyndhurst/Plan3_'+currentSlide3+'.jpg');
+      setTimeout(function(){
+        $('.slides3').slick({
+          infinite: true,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          prevArrow:"<img src='/assets/arrowleftimage.png' style='height:82px;position:absolute;margin-left:-24px;margin-top:0px;background-color:#000;opacity:.5'>",
+          nextArrow:"<img src='/assets/arrowrightimage.png' style='height:80px;position:absolute;margin-left:380px;margin-top:-81px;background-color:#000;opacity:.5'>",
+          mobileFirst: true,
+          variableWidth: false
+         // initialSlide: parseInt(currentSlide)
+          //focusOnSelect: true
+        });
+      },200);
+    });
+
+
+    $('.lnk4').on('click', function() {
+      $('#largeSlide4').modal('show');
+      $('.cplan4-main').attr('src','/assets/Plan4_Charlotte/Plan4_'+currentSlide4+'.jpg');
+      setTimeout(function(){
+        $('.slides4').slick({
+          infinite: true,
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          prevArrow:"<img src='/assets/arrowleftimage.png' style='height:82px;position:absolute;margin-left:-24px;margin-top:0px;background-color:#000;opacity:.5'>",
+          nextArrow:"<img src='/assets/arrowrightimage.png' style='height:80px;position:absolute;margin-left:380px;margin-top:-81px;background-color:#000;opacity:.5'>",
+          mobileFirst: true,
+          variableWidth: false
+         // initialSlide: parseInt(currentSlide)
+          //focusOnSelect: true
+        });
+      },200);
+    });
+
+    $('.lnk5').on('click', function() {
+      $('#largeSlide5').modal('show');
+      $('.cplan5-main').attr('src','/assets/Plan5_Aberdeen/Plan5_'+currentSlide5+'.jpg');
+      setTimeout(function(){
+        $('.slides5').slick({
+          infinite: true,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          prevArrow:"<img src='/assets/arrowleftimage.png' style='height:82px;position:absolute;margin-left:-24px;margin-top:0px;background-color:#000;opacity:.5'>",
+          nextArrow:"<img src='/assets/arrowrightimage.png' style='height:80px;position:absolute;margin-left:380px;margin-top:-81px;background-color:#000;opacity:.5'>",
+          mobileFirst: true,
+          variableWidth: false
+         // initialSlide: parseInt(currentSlide)
+          //focusOnSelect: true
+        });
+      },200);
+    });
+
+
+    $('.lnk6').on('click', function() {
+      $('#largeSlide6').modal('show');
+      $('.cplan6-main').attr('src','/assets/Plan6_Abigail/Plan6_'+currentSlide6+'.jpg');
+      setTimeout(function(){
+        $('.slides6').slick({
+          infinite: true,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          prevArrow:"<img src='/assets/arrowleftimage.png' style='height:82px;position:absolute;margin-left:-24px;margin-top:0px;background-color:#000;opacity:.5'>",
+          nextArrow:"<img src='/assets/arrowrightimage.png' style='height:80px;position:absolute;margin-left:380px;margin-top:-81px;background-color:#000;opacity:.5'>",
+          mobileFirst: true,
+          variableWidth: false
+         // initialSlide: parseInt(currentSlide)
+          //focusOnSelect: true
+        });
+      },200);
+    });
+
+
 
     $('.plana-main').on('click', function() {
-      //window.open($(this)[0].currentSrc, '', 'width=640,height=480');
-      //$('#largeSlide').modal('show');
-      //currentSlide = $('.slidesA').slick('slickCurrentSlide');
+      
       $('.lnk1').trigger("click");
       //$('.slides').resize();
     });
@@ -109,38 +187,114 @@ var currentSlideB = 1;
       $('.lnk2').trigger("click");
     });
       
+
+
+    $('.plan3-main').on('click', function() {
+      $('.lnk3').trigger("click");
+    });
+
+    $('.plan4-main').on('click', function() {
+      $('.lnk4').trigger("click");
+    });
+    $('.plan5-main').on('click', function() {
+      $('.lnk5').trigger("click");
+    });
+
+    $('.plan6-main').on('click', function() {
+      $('.lnk6').trigger("click");
+    });
+
+
     $('.lil-plana').on('click', function() {
       // console.log($(this));
       var rt = $(this).attr("id").replace(/\D/g,'');
       currentSlide = rt;
       // console.log(rt);
       if(rt!="") {
-        $('.plana-main')[0].src = '/assets/floorplanaimage'+rt+'.png';  
+        $('.plana-main')[0].src = '/assets/Plan1_Claire/'+rt+'.jpg';  
       }
       
+    });
+
+
+    $('.lil-plan5').on('click', function() {
+      // console.log($(this));
+      var rt = $(this).attr("id").replace(/\D/g,'');
+      currentSlideB = rt;
+      $('.plan5-main')[0].src = '/assets/Plan5_Aberdeen/Plan5_'+rt+'.jpg';
+    });
+
+    $('.lil-plan4').on('click', function() {
+      // console.log($(this));
+      var rt = $(this).attr("id").replace(/\D/g,'');
+      currentSlideB = rt;
+      $('.plan4-main')[0].src = '/assets/Plan4_Charlotte/Plan4_'+rt+'.jpg';
+    });
+
+    $('.lil-plan3').on('click', function() {
+      // console.log($(this));
+      var rt = $(this).attr("id").replace(/\D/g,'');
+      currentSlideB = rt;
+      $('.plan3-main')[0].src = '/assets/Plan3_Lyndhurst/Plan3_'+rt+'.jpg';
+    });
+
+
+    $('.lil-plan6').on('click', function() {
+      // console.log($(this));
+      var rt = $(this).attr("id").replace(/\D/g,'');
+      currentSlideB = rt;
+      $('.plan6-main')[0].src = '/assets/Plan6_Abigail/Plan6_'+rt+'.jpg';
     });
 
     $('.lil-planb').on('click', function() {
       // console.log($(this));
       var rt = $(this).attr("id").replace(/\D/g,'');
       currentSlideB = rt;
-      $('.planb-main')[0].src = '/assets/floorplanbimage'+rt+'.png';
+      $('.planb-main')[0].src = '/assets/Plan1_Claire/'+rt+'.jpg';
     });
 
     $('.wlil-plana').on('click', function() {
       // console.log($(this));
       var rt = $(this).attr("id").replace(/\D/g,'');
-      $('.wplana-main')[0].src = '/assets/floorplanabigimage'+rt+'.png';
+      $('.wplana-main')[0].src = '/assets/Plan1_Claire/'+rt+'.jpg';
     });
 
     $('.clil-planb').on('click', function() {
       
       var rt = $(this).attr("id").replace(/\D/g,'');
       console.log(rt);
-      $('.cplanb-main')[0].src = '/assets/floorplanbbigimage'+rt+'.png';
+      $('.cplanb-main')[0].src = '/assets/Plan2_Inverness/Plan2_'+rt+'.jpg';
     });
 
 
+    $('.clil-plan3').on('click', function() {
+      
+      var rt = $(this).attr("id").replace(/\D/g,'');
+      console.log(rt);
+      $('.cplan3-main')[0].src = '/assets/Plan3_Lyndhurst/Plan3_'+rt+'.jpg';
+    });
+
+    $('.clil-plan4').on('click', function() {
+      
+      var rt = $(this).attr("id").replace(/\D/g,'');
+      console.log(rt);
+      $('.cplan4-main')[0].src = '/assets/Plan4_Charlotte/Plan4_'+rt+'.jpg';
+    });
+
+    $('.clil-plan5').on('click', function() {
+      
+      var rt = $(this).attr("id").replace(/\D/g,'');
+      console.log(rt);
+      $('.cplan5-main')[0].src = '/assets/Plan5_Aberdeen/Plan5_'+rt+'.jpg';
+    });
+
+
+    $('.clil-plan6').on('click', function() {
+      
+      var rt = $(this).attr("id").replace(/\D/g,'');
+      console.log(rt);
+      $('.cplan6-main')[0].src = '/assets/Plan6_Abigail/Plan6_'+rt+'.jpg';
+    });
 
     // $('.plana').on('click', function() {
     	
